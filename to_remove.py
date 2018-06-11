edbf67762a87907666762fb4cf7ec017e617093c
vk <kurguzovv@kurguzovv>
2018-06-12 00:38:25 +0300

    test

--- to_remove.py
+++ to_remove.py
@@ -1,27 +1,40 @@
-'''
-data = (["X.O", "XX.", "XOO"])
+import json
+from collections import OrderedDict
 
-s1 = "x.o|xx.|xoo"
-s2 = "xxx|oxo|0.0"
+d = {}
+#
+print(d)
 
-for row in data:
-    print(row)
+with open('/opt/pythontests/mkb10/mkb/A00-B99_1.json') as j_data:
+	result = json.load(j_data)
+j_data.close()
 
-for i in range(len(data)):
-    print(i)
-
-for i in range(len(data)):
-    for row in data:
-        print(i,row[i])
-'''
-data = ["XXX", "XX.", "XOO"]
-
-def easyBreezy(row):
-    print(row[0])
-
-#print(all([x == 'X' for x in data[0]]))
-
-map(easyBreezy, data)
+# print(len(result))
+# print(len(code_urls))
+print("-----------------------------------------------")
 
 
-#print(list(filter(lambda x: x[0]=='X', data)))
+#print((result))
+
+# for category_number in range(0,len(result)):
+# 	sub = {}
+# 	for j in range(0,len(result[category_number]['the_code'])):
+# 		sub[result[category_number]['the_code'][j]] = result[category_number]['the_desc'][j]
+# 	d[code_urls[category_number]] = sub
+
+# print(d)
+
+for i in result:
+	sub = OrderedDict()
+	for j in range(0, len(i['code'])):
+		sub[i['code'][j]] = [i['description'][j]]
+	#print(sub)
+	#print("------------------------------------------------------------------------------------")
+	#print("{}-{}".format(sub.keys()[0],sub.keys()[-1]))
+	list_keys = list(sub.keys())
+	d["{}-{}".format(list_keys[0], list_keys[-1])] = sub
+#print(d)
+
+with open('/opt/pythontests/mkb10/mkb/A00-B99_1_4.json', 'w') as r_data:
+	json.dump(d, r_data, sort_keys=True, ensure_ascii=False)
+r_data.close()
